ccf9a059064d37c06b73ef252e34ce216bff0b77
ahuber93 <andrewhuber93@icloud.com>
2015-11-17 08:04:03 -0500

    Added class 'Step'

--- andrew_huber.lisp
+++ andrew_huber.lisp
@@ -18,6 +18,15 @@
 	)
 )
 
+(defclass my-step ()
+	(
+		(effect :accessor step-effect :initarg effect)
+		(instigator :accessor step-instigator :initarg instigator)
+		(victim :accessor step-victim :initarg victim)
+		(ai :accessor step-ai :initarg ai)
+		(cards :accessor step-cards :initarg cards)
+	)
+)
 
 (defun perform-move (player game)
 	(setq queue nil)
