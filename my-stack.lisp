f0615077f7e34a8cd49e9d8d0a4c9eaa4d1e9c8c
ahuber93 <andrewhuber93@icloud.com>
2015-12-10 09:14:17 -0500

    Stack works!

--- my-stack.lisp
+++ my-stack.lisp
@@ -5,16 +5,16 @@
 
 (defclass my-stack ()
 	(
-		(the-stack :accessor my-stack-the-stack :initarg the-stack)
+		(the-stack :accessor my-stack-the-stack :initarg the-stack :initform nil)
 	)
 )
 
 (defun push-to-stack (stack item)
-	(setq (slot-value stack 'the-stack) (append (list item) (my-stack-the-stack stack))))
+	(setf (slot-value stack 'the-stack) (append (list item) (my-stack-the-stack stack))))
 
-(defun pop-to-stack (stack)
-	(setq item (car (slot-value stack 'the-stack)))
-	(setq (slot-value stack 'the-stack) (cdr (slot-value stack 'the-stack)))
+(defun pop-from-stack (stack)
+	(setf item (car (slot-value stack 'the-stack)))
+	(setf (slot-value stack 'the-stack) (cdr (slot-value stack 'the-stack)))
 	item)
 
 (defun peek-from-stack (stack)
@@ -27,21 +27,21 @@
 	(eq 0 (list-length (my-stack-the-stack stack))))
 
 (defun xth-last-item-of-stack (stack x)
-	(setq copy (copy-stack stack))
-	(while (> (stack-size copy) x) do
+	(setf copy (copy-stack stack))
+	(loop while (> (stack-size copy) x) do
 		(pop-from-stack copy))
 	(pop-from-stack copy))
 
 (defun empty-stack (stack)
-	(setq (slot-value stack 'the-stack) nil))
+	(setf (slot-value stack 'the-stack) nil))
 
 (defun copy-stack (stack)
-	(setq temp (make-instance 'my-stack))
-	(setq new (make-instance 'my-stack))
-	(while (not (is-empty stack)) do
+	(setf temp (make-instance 'my-stack))
+	(setf new (make-instance 'my-stack))
+	(loop while (not (is-empty stack)) do
 		(push-to-stack temp (pop-from-stack stack)))
-	(while (not (is-empty temp)) do
-		(setq data (pop-from-stack temp))
+	(loop while (not (is-empty temp)) do
+		(setf data (pop-from-stack temp))
 		(push-to-stack stack data)
 		(push-to-stack new data))
 	new)
